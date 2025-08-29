f5198803b12f371d6eb84cf7cb814bd009577856
11102Jamd <jalejandromd17@gmail.com>
2025-08-29 10:31:49 -0500

    Cambio para decimales y relaciones

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -35,7 +35,7 @@ class ProductionController extends Controller
     public function show($id)
     {
         try {
-            $production = Production::with(['consumptions.input', 'consumptions.batch'])->findOrFail($id);
+            $production = Production::with(['consumptions.input', 'consumptions.batch', 'recipe'])->findOrFail($id);
             return response()->json($production);
         } catch (\Throwable $th) {
             return response()->json([
